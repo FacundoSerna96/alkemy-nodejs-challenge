cf66024c9c3aa0e998e7960e501740f3a40aad31
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-07-03 18:52:04 -0300

    Se agrega "Modificar pelicula" (PUT
    -Corrección en el POST de película.

--- controllers/movie.js
+++ controllers/movie.js
@@ -31,7 +31,7 @@ const movieGetOne = async (req = request, res = response) => {
         res.status(200).json({
             movie : movie
         })
-        
+
     } catch (error) {
         res.status(400).json({
             error : error
@@ -42,17 +42,19 @@ const movieGetOne = async (req = request, res = response) => {
 
 const moviePost = async (req = request, res = response) => {
 
-    const {title, image, releaseDate, rating, genreId, state} = req.body;
+    const {title, image, releaseDate, rating, genreId} = req.body;
+
+    const state = 1;
 
     const newMovie = new Movie({title, image, releaseDate, rating, genreId, state})
 
     try {
         await newMovie.save()
-        res.status(201).json({
+        return res.status(201).json({
             movie: newMovie
         })
     } catch (error) {
-        res.status(400).json({
+        return res.status(400).json({
             error: error
         })
     }
@@ -61,9 +63,39 @@ const moviePost = async (req = request, res = response) => {
 
 
 const moviePut = async (req = request, res = response) => {
-    res.json({
-        msg: 'movie update'
-    })
+
+    const {id} = req.params;
+    const {title, image, releaseDate, rating, genreId} = req.body;
+
+    const updatedMovie = {
+        title: title,
+        image: image,
+        releaseDate: releaseDate,
+        rating: rating,
+        genreId : genreId
+    }
+
+    try {
+        const movie = await Movie.findByPk(id)
+        
+        if(!movie){
+            return res.status(404).json({
+                msg:`movie not found ${id}`
+            })
+        }
+
+        await movie.update(updatedMovie);
+
+        return res.status(200).json({
+            movie: movie
+        })
+
+    } catch (error) {
+        return res.status(400).json({
+            error: error
+        })
+    }
+
 }
 
 

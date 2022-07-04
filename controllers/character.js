d1cd921fc099e15eff78b729986ebdff3fb5d3b7
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-07-04 00:45:37 -0300

    Se agrega "Obtener un personaje" (GET)
    También devuelve las películas donde el personaje aparece.

--- controllers/character.js
+++ controllers/character.js
@@ -13,9 +13,34 @@ const characterGet = async (req = request, res = response) => {
 
 const characterGetOne = async (req = request, res = response) => {   
     
-    return res.status(200).json({
-        msg : 'character get one'
-    })
+    const {id} = req.params;
+
+    try {
+        const character = await Character.findOne({
+            where: {
+                id: id,
+                state: 1
+            }
+        })
+
+        const idMovie = character.movieId;
+
+        const movies = await Movie.findAll({
+            where : {
+                id: idMovie
+            }
+        })
+    
+        res.status(200).json({
+            character : character,
+            movies: movies
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            error : error
+        })
+    }
 }
 
 
@@ -78,14 +103,58 @@ const characterPut = async (req = request, res = response) => {
 
 //virtual DELETE
 const characterDelete = async (req = request, res = response) => {
-    return res.status(200).json({
-        msg : 'character delete'
-    })
+    const {id} = req.params;
+    
+    const updatedCharacter = {
+        state: false
+    }
+
+    try {
+        const character = await Character.findByPk(id)
+        
+        if(!character){
+            return res.status(404).json({
+                msg:`character not found ${id}`
+            })
+        }
+
+        await character.update(updatedCharacter);
+
+        return res.status(200).json({
+            character: character
+        })
+
+    } catch (error) {
+        return res.status(400).json({
+            error: error
+        })
+    }
 }
 
 //phisical DELETE
 const characterPhisicalDelete = async (req = request, res = response) => {
+    const {id} = req.params;
     
+    try {
+        const character = await Character.findByPk(id)
+        
+        if(!character){
+            return res.status(404).json({
+                msg:`character not found ${id}`
+            })
+        }
+
+        await character.destroy();
+
+        return res.status(200).json({
+            msg: 'Character Deleted'
+        })
+
+    } catch (error) {
+        return res.status(400).json({
+            error: error
+        })
+    }
 }
 
 
